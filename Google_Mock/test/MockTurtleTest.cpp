892b35952f59ad7c724e57cb06d702be7eefebae
Nivedita Rajan <nivedita218@gmail.com>
2017-07-12 11:36:05 +0200

    Google Mock Example

---
+++ Google_Mock/test/MockTurtleTest.cpp
@@ -0,0 +1,22 @@
+#include "MockTurtle.h"
+#include "Painter.h"
+
+#include "gtest/gtest.h"
+
+using ::testing::AtLeast;
+TEST(PainterTest, CanDrawSomething) {
+MockTurtle turtle;
+EXPECT_CALL(turtle, PenDown())
+.Times(AtLeast(1));
+
+Painter painter(&turtle);
+
+EXPECT_TRUE(painter.DrawCircle(0, 0, 10));
+}
+
+int main(int argc, char** argv) {
+    // The following line must be executed to initialize Google Mock
+    // (and Google Test) before running the tests.
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
